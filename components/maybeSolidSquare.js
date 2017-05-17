ee99157d9bca261b048a6bbf82c86b18e251e886
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-17 17:27:46 +0900

    refactor shared square/tile code
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/maybeSolidSquare.js
+++ components/maybeSolidSquare.js
@@ -1,16 +1,16 @@
 import { COLOR_A, COLOR_B, SQUARE_SIZE } from '../../shared/common/customize'
-import drawStripedSquare from '../../shared/render/drawStripedSquare'
+import tile from '../../shared/components/tile'
 
 export default ({x, y}) => {
-	if (x % 2 == 1 && y % 2 == 0) {
-		drawStripedSquare({
+	if (x % 2 === 1 && y % 2 === 0) {
+		tile({
 			origin: [ x * SQUARE_SIZE, y * SQUARE_SIZE ],
 			size: SQUARE_SIZE,
 			originColor: COLOR_A,
 			otherColor: COLOR_A
 		})
-	} else if (x % 2 == 0 && y % 2 == 1) {
-		drawStripedSquare({
+	} else if (x % 2 === 0 && y % 2 === 1) {
+		tile({
 			origin: [ x * SQUARE_SIZE, y * SQUARE_SIZE ],
 			size: SQUARE_SIZE,
 			originColor: COLOR_B,
