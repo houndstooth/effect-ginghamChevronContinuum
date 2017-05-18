dce728cdabc24fc6bc932c765dd9a5d7790379c2
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-18 17:16:23 +0900

    merge more code into shared tile
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/gccMaybeSolidTile.js
+++ components/gccMaybeSolidTile.js
@@ -1,13 +1,3 @@
-import { SQUARE_SIZE } from '../../shared/common/customize'
 import tile from '../../shared/components/tile'
-import calculateColors from '../../shared/utilities/calculateColors'
 
-export default ({ x, y, options }) => {
-	if (x % 2 !== 0 || y % 2 !== 0) return
-
-	const { originColor, otherColor } = calculateColors({ x, y })
-	const origin = [ x * SQUARE_SIZE, y * SQUARE_SIZE ]
-	const size = SQUARE_SIZE
-
-	tile({ origin, size, originColor, otherColor, options })
-}
+export default ({ x, y }) => { if (x % 2 === y % 2) tile({ x, y }) }
--- components/harmonicShrinkingTile.js
+++ components/harmonicShrinkingTile.js
@@ -1,12 +1,3 @@
-import { SQUARE_SIZE } from '../../shared/common/customize'
 import tile from '../../shared/components/tile'
-import calculateColors from '../../shared/utilities/calculateColors'
 
-export default ({ x, y, options }) => {
-	const { harmonicShrinkingStripes } = options
-	const { originColor, otherColor } = calculateColors({ x, y })
-	const origin = [ x * SQUARE_SIZE, y * SQUARE_SIZE ]
-	const size = SQUARE_SIZE
-
-	tile({ origin, size, originColor, otherColor, harmonicShrinkingStripes })
-}
+export default ({ x, y, options }) => tile({ x, y, harmonicShrinkingStripes: options.harmonicShrinkingStripes })
