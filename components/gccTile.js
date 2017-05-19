9e4923befe5f93d90f7698e8548f3fff7c3fd2cb
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-19 14:55:09 +0900

    standardize use of origin instead of xy
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/gccTile.js
+++ components/gccTile.js
@@ -1,15 +1,11 @@
 import tile from '../../shared/components/tile'
 import { CONTINUUM_STARTS_AT_STRIPE_COUNT, STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL } from '../common/customize'
 
-export default ({ x, y }) => {
+export default ({ origin }) => {
 	const stripeCount = Math.floor(
-		((x + y + (CONTINUUM_STARTS_AT_STRIPE_COUNT * 2)) * STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL / 2)
+		((origin[ 0 ] + origin[ 1 ] + (CONTINUUM_STARTS_AT_STRIPE_COUNT * 2)) * STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL / 2)
 		- (CONTINUUM_STARTS_AT_STRIPE_COUNT * (STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL - 1))
 	)
 
-	tile({
-		x,
-		y,
-		stripeCount
-	})
+	tile({ origin, stripeCount })
 }
