b0d6799a25b495baf388b2ac17c1e83e1dad546c
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-19 13:45:22 +0900

    return thinning rate type control to gcc, in a nicer way this time
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- components/gccTile.js
+++ components/gccTile.js
@@ -1,3 +1,15 @@
 import tile from '../../shared/components/tile'
+import { CONTINUUM_STARTS_AT_STRIPE_COUNT, STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL } from '../common/customize'
 
-export default ({x, y}) => tile({x, y, stripeCount: Math.floor((x + y + 2) / 2)})
+export default ({ x, y }) => {
+	const stripeCount = Math.floor(
+		((x + y + (CONTINUUM_STARTS_AT_STRIPE_COUNT * 2)) * STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL / 2)
+		- (CONTINUUM_STARTS_AT_STRIPE_COUNT * (STRIPE_COUNT_INCREASES_BY_X_EACH_DIAGONAL - 1))
+	)
+
+	tile({
+		x,
+		y,
+		stripeCount
+	})
+}
