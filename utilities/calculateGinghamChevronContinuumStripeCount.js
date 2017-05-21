45e1fb7e093bc47a0cd6c1e9abef538356a73a7a
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-21 16:03:14 +0900

    stuff in here is relevant to aligning, aka user friendly static mode

--- utilities/calculateGinghamChevronContinuumStripeCount.js
+++ utilities/calculateGinghamChevronContinuumStripeCount.js
@@ -1,7 +1,7 @@
 import state from '../../state'
 
 export default ({ origin }) => {
-    const { continuumStartsAtStripeCount, stripeCountIncreasePerDiagonal } = state.shared.stripeCount.ginghamChevronContinuum
+    const { continuumStartsAtStripeCount, stripeCountIncreasePerDiagonal } = state.shared.stripeCount.ginghamChevronContinuum.aligning
     return Math.floor(
         ((origin[ 0 ] + origin[ 1 ] + (continuumStartsAtStripeCount * 2)) * stripeCountIncreasePerDiagonal / 2)
         - (continuumStartsAtStripeCount * (stripeCountIncreasePerDiagonal - 1))
