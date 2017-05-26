cf881b08d06faaea210552a60272e4e5d76dc99f
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-05-26 18:04:19 +0900

    no longer need this
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- calculateGinghamChevronContinuumStripeCount.js
+++
@@ -1,9 +0,0 @@
-import state from '../shared/state/state'
-
-export default ({ origin }) => {
-	const { continuumStartsAtStripeCount, stripeCountIncreasePerDiagonal } = state.shared.stripeCount.ginghamChevronContinuum.aligning
-	return Math.floor(
-		((origin[ 0 ] + origin[ 1 ] + (continuumStartsAtStripeCount * 2)) * stripeCountIncreasePerDiagonal / 2)
-		- (continuumStartsAtStripeCount * (stripeCountIncreasePerDiagonal - 1))
-	)
-}
