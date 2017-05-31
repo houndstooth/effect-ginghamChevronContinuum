07c95d8a78ee29b8f640f3fe04da4734fca3ae99
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-05-31 23:55:48 +0900

    generalize realignment from just colors to both colors and orientation, for dazzle

--- maybeRealign.js
+++ maybeRealign.js
@@ -2,19 +2,19 @@ import neededStripeCountToCoverGrid from './neededStripeCountToCoverGrid'
 import state from '../shared/state/state'
 import trapezoidalNumbers from './trapezoidalNumbers'
 
-export default ({ colors, origin }) => {
+export default ({ setForTile, origin }) => {
 	const { initial, delta } = state.shared.stripeCount.ginghamChevronContinuum
 
 	for (let n = 0; n < neededStripeCountToCoverGrid(); n++) {
 		const thisStripe = trapezoidalNumbers.trapezoidalRoot({ initial, delta, n }) * 2
 		if (thisStripe > origin[ 0 ] + origin[ 1 ]) {
 			if (n % 2 === 1) {
-				return colors.reverse()
+				return setForTile.reverse()
 			} else {
-				return colors
+				return setForTile
 			}
 		}
 	}
 
-	return colors
+	return setForTile
 }
