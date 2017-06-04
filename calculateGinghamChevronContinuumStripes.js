464ce26ff2a366f941ffb5fa577e20f26696af72
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-06-04 17:18:01 +0900

    this makes sense and makes it nicer with morphosis

--- calculateGinghamChevronContinuumStripes.js
+++ calculateGinghamChevronContinuumStripes.js
@@ -5,7 +5,7 @@ import mathUtilities from '../shared/utilities/mathUtilities'
 export default ({ address }) => {
 	const { initial, delta } = state.stripeCountConfig.ginghamChevronContinuum
 	let stripes = [ 0 ]
-	const distanceFromOrigin = Math.abs(address[ 0 ] + address[ 1 ])
+	const distanceFromOrigin = Math.abs(address[ 0 ]) + Math.abs(address[ 1 ])
 
 	for (let n = 0; n < neededStripeCountToCoverGrid(); n++) {
 		const stripe = mathUtilities.termialRoot({ initial, delta, n }) * 2
