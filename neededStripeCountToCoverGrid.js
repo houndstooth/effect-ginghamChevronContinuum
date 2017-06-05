f29ca86f8d7b9a7b26eaa31e46795dcc4ec98bbe
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-05 16:11:19 +0900

    exports to top
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- neededStripeCountToCoverGrid.js
+++ neededStripeCountToCoverGrid.js
@@ -2,7 +2,7 @@ import state from '../../state/state'
 import mathUtilities from '../../utilities/mathUtilities'
 
 export default () => {
-    const { gridSize, stripeCountConfig } = state
-    const { initial, delta } = stripeCountConfig.ginghamChevronContinuum
-    return initial + delta * mathUtilities.triangularNumber(gridSize)
+	const { gridSize, stripeCountConfig } = state
+	const { initial, delta } = stripeCountConfig.ginghamChevronContinuum
+	return initial + delta * mathUtilities.triangularNumber(gridSize)
 }
