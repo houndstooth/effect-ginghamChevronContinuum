8eb0cd3128a933f18c8fd6fb8982f8c58565232e
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-06 16:11:19 +0900

    clarify origin
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- getGinghamChevronContinuumStripePositions.js
+++ getGinghamChevronContinuumStripePositions.js
@@ -5,15 +5,15 @@ import mathUtilities from '../../utilities/mathUtilities'
 export default ({ address }) => {
 	const { initial, delta } = state.stripeCountConfig.ginghamChevronContinuum
 	let stripes = [ 0 ]
-	const distanceFromOrigin = Math.abs(address[ 0 ]) + Math.abs(address[ 1 ])
+	const distanceFromZeroZeroAddress = Math.abs(address[ 0 ]) + Math.abs(address[ 1 ])
 
 	for (let n = 0; n < neededStripeCountToCoverGrid(); n++) {
 		const stripe = mathUtilities.termialRoot({ initial, delta, n }) * 2
-		if (stripe >= distanceFromOrigin + 2) {
+		if (stripe >= distanceFromZeroZeroAddress + 2) {
 			return stripes
 		}
-		if (stripe > distanceFromOrigin) {
-			stripes.push((stripe - distanceFromOrigin) % 2)
+		if (stripe > distanceFromZeroZeroAddress) {
+			stripes.push((stripe - distanceFromZeroZeroAddress) % 2)
 		}
 	}
 
