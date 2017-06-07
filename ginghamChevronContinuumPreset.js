18f2cb5beb1647ce9e1749db8814b5d951fd6319
Person McPivot <person.mcpivot@pivotal.io>
2017-06-07 10:41:16 +0900

    Revert "well on the right track but major yikes"
    This reverts commit e3c0690511853a287464c4ed4b72b0716f37d0a8.

--- ginghamChevronContinuumPreset.js
+++ ginghamChevronContinuumPreset.js
@@ -1,8 +1,6 @@
 import { ANIMATION_RATE } from '../../application/constants'
 import realignSetForTileToMaintainGinghamChevronContinuumAcrossGrid from './realignSetForTileToMaintainGinghamChevronContinuumAcrossGrid'
 import getGinghamChevronContinuumStripePositions from './getGinghamChevronContinuumStripePositions'
-import state from '../../state/state'
-import currentAnimation from '../../state/currentAnimation'
 
 export default {
 	state: {
@@ -23,8 +21,8 @@ export default {
 	animations: {
 		stripeCountConfig: {
 			ginghamChevronContinuum: {
-				initial: () => state.stripeCountConfig.ginghamChevronContinuum.initial * Math.pow(ANIMATION_RATE, currentAnimation.i),
-				delta: () => state.stripeCountConfig.ginghamChevronContinuum.delta * Math.pow(ANIMATION_RATE, currentAnimation.i)
+				initial: p => p * ANIMATION_RATE,
+				delta: p => p * ANIMATION_RATE
 			}
 		}
 	}
