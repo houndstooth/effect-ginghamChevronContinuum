75fa22ab9d3eceff725c9f05720cd3f2865f23b4
Douglas Blumeyer <dblumeyer@pivotal.io>
2017-06-27 10:14:35 +0900

    fix gcc
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- neededStripeCountToCoverGrid.js
+++ neededStripeCountToCoverGrid.js
@@ -1,7 +1,9 @@
 import mathUtilities from '../../utilities/mathUtilities'
+import { GRID_SIZE } from '../../defaults'
 
 export default () => {
-	const { gridConfig: { gridSize }, stripeCountConfig } = settings.initial
+	const { gridConfig, stripeCountConfig } = settings.initial
+	const { gridSize } = gridConfig || { gridSize: GRID_SIZE }
 	const { initial, delta } = stripeCountConfig.ginghamChevronContinuum
 	return initial + delta * mathUtilities.triangularNumber(gridSize)
 }
