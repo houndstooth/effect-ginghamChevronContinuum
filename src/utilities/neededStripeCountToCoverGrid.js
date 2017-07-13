de53a926f9dacbb4394d52124ff358f8cec610b9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-13 14:06:23 +0900

    helper for defaulting objects that might be undefined on settings, plus extract activation of test marker

--- src/utilities/neededStripeCountToCoverGrid.js
+++ src/utilities/neededStripeCountToCoverGrid.js
@@ -1,9 +1,13 @@
 import mathUtilities from '../../../../src/utilities/mathUtilities'
+import settingsUtilities from '../../../../src/utilities/settingsUtilities'
 import { GRID_SIZE } from '../../../../src/defaults'
 
 export default () => {
-	const { gridSettings, stripeCountSettings } = current.settings.initial
-	const { gridSize } = gridSettings || { gridSize: GRID_SIZE }
+	const { stripeCountSettings } = current.settings.initial
+	const gridSize = settingsUtilities.getFromSettingsOrDefault({
+		nestedPropertyPath: [ 'initial', 'gridSettings', 'gridSize' ],
+		defaultForProperty: GRID_SIZE,
+	})
 	const { initial, delta } = stripeCountSettings.ginghamChevronContinuum
 	return initial + delta * mathUtilities.triangularNumber(gridSize)
 }
