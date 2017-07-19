d777902903262462e7f64148ca92c266e55c0b8b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-07-19 17:13:19 +0900

    improve defaults by merging them on in the setup step like it used to be, so you can assume they are there in most cases; this way you expect to call setup in beforeEach of more tests and then do not have to test for the defaulting; associate default values with their positions in the settings structure like it used to be; extract setupCanvas like it used to be; return early if any effect has any unrecognized settings objects so it is like defaults and overrides and current in that respect; fix cmyktooth bug due to destructuring before imports were ready; create settings directory to house all this new settings-specific infrastructure; test overrides; caught some missing settings to register

--- src/utilities/neededStripeCountToCoverGrid.js
+++ src/utilities/neededStripeCountToCoverGrid.js
@@ -1,13 +1,7 @@
 import mathUtilities from '../../../../src/utilities/mathUtilities'
-import settingsUtilities from '../../../../src/utilities/settingsUtilities'
-import { GRID_SIZE } from '../../../../src/defaults'
 
 export default () => {
-	const { stripeCountSettings } = current.settings.initial
-	const gridSize = settingsUtilities.getFromSettingsOrDefault({
-		nestedPropertyPath: [ 'initial', 'gridSettings', 'gridSize' ],
-		defaultForProperty: GRID_SIZE,
-	})
+	const { stripeCountSettings, gridSettings } = current.settings.initial
 	const { initial, delta } = stripeCountSettings.ginghamChevronContinuum
-	return initial + delta * mathUtilities.triangularNumber(gridSize)
+	return initial + delta * mathUtilities.triangularNumber(gridSettings.gridSize)
 }
--- test/effects/ginghamChevronContinuumEffectTest.js
+++ test/effects/ginghamChevronContinuumEffectTest.js
@@ -1,31 +1,112 @@
 import execute from '../../../../src/application/execute'
-import setup from '../../../../src/application/setup'
+import setup from '../../../../src/settings/setup'
 import tileSectorCenterIsColor from '../../../../test/helpers/tileSectorCenterIsColor'
 import activateTestMarkerCanvas from '../../../../test/helpers/activateTestMarkerCanvas'
 import { BLACK, TRANSPARENT } from '../../../../src/constants'
-import { TILE_SIZE } from '../../../../src/defaults'
 import ginghamChevronContinuumEffect from '../../effects/ginghamChevronContinuumEffect'
+import settingsUtilities from '../../../../src/utilities/settingsUtilities'
+import settingsPaths from '../../../../src/settings/settingsPaths'
 
 describe('gingham chevron continuum effect', () => {
 	it('each new diagonal row has an extra stripe', () => {
-		const tileSizeInPixels = TILE_SIZE
+		const tileSizeInPixels = settingsUtilities.getFromSettingsOrDefault(settingsPaths.TILE_SIZE)
 		setup({ effects: [ ginghamChevronContinuumEffect ] })
 		activateTestMarkerCanvas()
 
 		execute()
 
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 0 * tileSizeInPixels, 0 * tileSizeInPixels ], tileSizeInPixels, x: 0, y: 0, n: 1, color: BLACK })).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 0 * tileSizeInPixels, 0 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 0,
+			y: 0,
+			n: 1,
+			color: BLACK,
+		})).toBe(true)
 
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 1 * tileSizeInPixels, 1 * tileSizeInPixels ], tileSizeInPixels, x: 0, y: 0, n: 2, color: BLACK })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 1 * tileSizeInPixels, 1 * tileSizeInPixels ], tileSizeInPixels, x: 1, y: 1, n: 2, color: TRANSPARENT })).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 1 * tileSizeInPixels, 1 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 0,
+			y: 0,
+			n: 2,
+			color: BLACK,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 1 * tileSizeInPixels, 1 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 1,
+			y: 1,
+			n: 2,
+			color: TRANSPARENT,
+		})).toBe(true)
 
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ], tileSizeInPixels, x: 0, y: 0, n: 3, color: TRANSPARENT })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ], tileSizeInPixels, x: 1, y: 1, n: 3, color: BLACK })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ], tileSizeInPixels, x: 2, y: 2, n: 3, color: TRANSPARENT })).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 0,
+			y: 0,
+			n: 3,
+			color: TRANSPARENT,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 1,
+			y: 1,
+			n: 3,
+			color: BLACK,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 2 * tileSizeInPixels, 2 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 2,
+			y: 2,
+			n: 3,
+			color: TRANSPARENT,
+		})).toBe(true)
 
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ], tileSizeInPixels, x: 0, y: 0, n: 4, color: TRANSPARENT })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ], tileSizeInPixels, x: 1, y: 1, n: 4, color: BLACK })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ], tileSizeInPixels, x: 2, y: 2, n: 4, color: TRANSPARENT })).toBe(true)
-		expect(tileSectorCenterIsColor({ id: 1, originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ], tileSizeInPixels, x: 3, y: 3, n: 4, color: BLACK })).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 0,
+			y: 0,
+			n: 4,
+			color: TRANSPARENT,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 1,
+			y: 1,
+			n: 4,
+			color: BLACK,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 2,
+			y: 2,
+			n: 4,
+			color: TRANSPARENT,
+		})).toBe(true)
+		expect(tileSectorCenterIsColor({
+			id: 1,
+			originInPixels: [ 3 * tileSizeInPixels, 3 * tileSizeInPixels ],
+			tileSizeInPixels,
+			x: 3,
+			y: 3,
+			n: 4,
+			color: BLACK,
+		})).toBe(true)
 	})
 })
