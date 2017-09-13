9f2b61a5f1c5937d829be12bc7fe7b943f603692
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-13 23:22:46 +0900

    bon voyage, rewire, you have screwed me for the third time this week, and now i have a way around you, importing * to create an artificial object wrapping the default export function module for test and then spying on the default property of it

--- test/src/components/realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGridTest.js
+++ test/src/components/realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGridTest.js
@@ -1,6 +1,7 @@
 import state from '../../../../../state'
 import resetState from '../../../../../src/store/resetState'
 import composeMainHoundstooth from '../../../../../src/execute/composeMainHoundstooth'
+import * as indexOfFirstGridStripeCrossingThisTile from '../../../src/components/indexOfFirstGridStripeCrossingThisTile'
 import subject from '../../../src/components/realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGrid'
 
 describe('realign tile color indices to maintain gingham chevron continuum across grid, because the information about alternation of the colorSet has been lost from individual tiles, we have to check the modulus of the overall grid stripes instead', () => {
@@ -27,14 +28,14 @@ describe('realign tile color indices to maintain gingham chevron continuum acros
 
 	describe('when the index of the first grid stripe crossing this tile is even', () => {
 		it('returns the set for tile as is; all is well', () => {
-			subject.__Rewire__('indexOfFirstGridStripeCrossingThisTile', () => 2)
+			spyOn(indexOfFirstGridStripeCrossingThisTile, 'default').and.returnValue(2)
 			expect(subject({ tileColorIndices, gridAddress })).toEqual([ 'a', 'b' ])
 		})
 	})
 
 	describe('when the index of the first grid stripe crossing this tile is odd', () => {
 		it('reverses the set to flip the grain, to realign with previous diagonal row of striped tiles', () => {
-			subject.__Rewire__('indexOfFirstGridStripeCrossingThisTile', () => 1)
+			spyOn(indexOfFirstGridStripeCrossingThisTile, 'default').and.returnValue(1)
 			expect(subject({ tileColorIndices, gridAddress })).toEqual([ 'b', 'a' ])
 		})
 	})
