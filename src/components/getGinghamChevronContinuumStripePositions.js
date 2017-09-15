1fd42ee98463012b98cbf1069c167ffcea330efa
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2017-09-15 13:11:22 +0900

    zero zero address is now home address

--- src/components/getGinghamChevronContinuumStripePositions.js
+++ src/components/getGinghamChevronContinuumStripePositions.js
@@ -1,22 +1,22 @@
 import neededStripeCountToCoverGrid from './neededStripeCountToCoverGrid'
 import mathUtilities from '../../../../src/utilities/mathUtilities'
 import state from '../../../../state'
-import getDistanceFromZeroZeroAddress from './getDistanceFromZeroZeroAddress'
+import getDistanceFromHomeAddress from './getDistanceFromHomeAddress'
 
 export default ({ gridAddress }) => {
 	const { initialStripeCount, deltaStripeCount } = state.mainHoundstooth.basePattern.stripeSettings.stripePositionSettings.stripeCountContinuumSettings
 	let stripes = [ 0 ]
-	const distanceFromZeroZeroAddress = getDistanceFromZeroZeroAddress({ gridAddress })
+	const distanceFromHomeAddress = getDistanceFromHomeAddress({ gridAddress })
 
 	for (let n = 0; n < neededStripeCountToCoverGrid(); n++) {
 		const stripe = mathUtilities.termialRoot(
 			{ rangeStart: initialStripeCount, rangeDelta: deltaStripeCount, n }
 		) * 2
-		if (stripe >= distanceFromZeroZeroAddress + 2) {
+		if (stripe >= distanceFromHomeAddress + 2) {
 			return stripes
 		}
-		if (stripe > distanceFromZeroZeroAddress) {
-			stripes.push((stripe - distanceFromZeroZeroAddress) % 2)
+		if (stripe > distanceFromHomeAddress) {
+			stripes.push((stripe - distanceFromHomeAddress) % 2)
 		}
 	}
 
---
+++ test/src/components/getDistanceFromHomeAddressTest.js
@@ -0,0 +1,19 @@
+import getDistanceFromHomeAddress from '../../../src/components/getDistanceFromHomeAddress'
+
+describe('get distance from home address', () => {
+	it('gives the distance, in address positions, from grid address 0, 0', () => {
+		expect(getDistanceFromHomeAddress({ gridAddress: [ 0, 0 ] })).toBe(0)
+		expect(getDistanceFromHomeAddress({ gridAddress: [ 3, 0 ] })).toBe(3)
+		expect(getDistanceFromHomeAddress({ gridAddress: [ 0, 5 ] })).toBe(5)
+		expect(getDistanceFromHomeAddress({ gridAddress: [ 2, 7 ] })).toBe(9)
+	})
+
+	it('works for addresses of more than 2 dimensions', () => {
+		expect(getDistanceFromHomeAddress({ gridAddress: [ 1, 2, 3 ] })).toBe(6)
+	})
+
+	it('gives absolute distance, so it works for addresses with negative values', () => {
+		expect(getDistanceFromHomeAddress({ gridAddress: [ -3, -2 ] })).toBe(5)
+		expect(getDistanceFromHomeAddress({ gridAddress: [ -3, 2 ] })).toBe(5)
+	})
+})
