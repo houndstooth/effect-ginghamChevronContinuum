fcd834386819afab71c88d53279b92f21de5f71d
Douglas <douglas.blumeyer@gmail.com>
2017-09-28 23:31:01 +0800

    randomize unit test suite, revealing some weak design decisions; seize opportunity to add a global resetState

--- src/components/realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGrid.js
+++ src/components/realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGrid.js
@@ -1,8 +1,9 @@
 import indexOfFirstGridStripeCrossingThisTile from './indexOfFirstGridStripeCrossingThisTile'
+import { reversed } from '../../../../src/utilities/codeUtilities'
 
 const realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGrid = ({ tileColorIndices, gridAddress }) => {
 	const stripeIndex = indexOfFirstGridStripeCrossingThisTile({ gridAddress })
-	return stripeIndex % 2 === 1 ? tileColorIndices.reverse() : tileColorIndices
+	return stripeIndex % 2 === 1 ? reversed(tileColorIndices) : tileColorIndices
 }
 
 export default realignTileColorIndicesToMaintainGinghamChevronContinuumAcrossGrid
