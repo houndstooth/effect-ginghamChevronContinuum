b785d33dfabe122ee91f62888f35ba07022f1c02
Douglas <douglas.blumeyer@gmail.com>
2017-10-21 18:20:55 +0800

    naming consistency - singular type, singular name

--- test/integration/helpers/expectStripedTile.ts
+++ test/integration/helpers/expectStripedTile.ts
@@ -1,4 +1,4 @@
-import { Color, from, to, Units } from '../../../../../src'
+import { Color, from, to, Unit } from '../../../../../src'
 import { BLACK, TRANSPARENT } from '../../../../../src/constants'
 import { iterator } from '../../../../../src/utilities/codeUtilities'
 import { getFromBasePatternOrDefault } from '../../../../../test/helpers/getFromBasePatternOrDefault'
@@ -7,12 +7,12 @@ import { sectionCenterIsColor } from '../../../../../test/integration/helpers/se
 
 type ExpectStripedTile = (_: { diagonalAddress: number, firstColor: Color, stripeCount: number }) => void
 const expectStripedTile: ExpectStripedTile = ({ diagonalAddress, firstColor, stripeCount }) => {
-	const areaSize: Units = getFromBasePatternOrDefault(TILE_SIZE)
+	const areaSize: Unit = getFromBasePatternOrDefault(TILE_SIZE)
 	iterator(stripeCount).forEach(stripe => {
 		expect(sectionCenterIsColor({
 			areaOrigin: to.Coordinate([
-				diagonalAddress * from.Units(areaSize),
-				diagonalAddress * from.Units(areaSize),
+				diagonalAddress * from.Unit(areaSize),
+				diagonalAddress * from.Unit(areaSize),
 			]),
 			areaSize,
 			color: stripe % 2 === 0 ? firstColor : firstColor === BLACK ? TRANSPARENT : BLACK,
