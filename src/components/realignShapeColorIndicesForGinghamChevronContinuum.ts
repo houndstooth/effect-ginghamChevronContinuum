b3a89cc29385204d21fae03ea065546e4d34b68f
Douglas <douglas.blumeyer@gmail.com>
2017-10-25 03:48:27 +0800

    DRY up call signature types, and use them where you can

--- src/components/realignShapeColorIndicesForGinghamChevronContinuum.ts
+++ src/components/realignShapeColorIndicesForGinghamChevronContinuum.ts
@@ -1,11 +1,10 @@
-import { Address, ShapeColorIndex, to } from '../../../../src'
+import { to, TransformShapeColorIndices } from '../../../../src'
 import { reversed } from '../../../../src/utilities/codeUtilities'
 import { isOdd } from '../../../../src/utilities/mathUtilities'
 import { indexOfFirstStripeCrossingThisTile } from './indexOfFirstStripeCrossingThisTile'
 
-const realignShapeColorIndicesForGinghamChevronContinuum: (_: {
-	gridAddress: Address[], shapeColorIndices: ShapeColorIndex[],
-}) => ShapeColorIndex[] = ({ gridAddress, shapeColorIndices }) => {
+const realignShapeColorIndicesForGinghamChevronContinuum: TransformShapeColorIndices = params => {
+	const { gridAddress, shapeColorIndices } = params
 	const stripeIndex = indexOfFirstStripeCrossingThisTile({ gridAddress })
 
 	const realignedShapeColorIndices = isOdd(stripeIndex) ? reversed(shapeColorIndices) : shapeColorIndices
