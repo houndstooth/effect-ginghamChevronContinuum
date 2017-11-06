5e103786ada0096283e2ca18924a7351a8ac1e0c
Douglas <douglas.blumeyer@gmail.com>
2017-11-06 19:28:28 +0800

    do not await if you do not care

--- test/integration/effects/ginghamChevronContinuumEffectTest.ts
+++ test/integration/effects/ginghamChevronContinuumEffectTest.ts
@@ -18,85 +18,87 @@ describe('gingham chevron continuum effect', () => {
 		state.selectedHoundstoothEffects = [ ginghamChevronContinuumEffect ]
 		activateTestMarkerCanvas()
 
-		await executeSelectedHoundstoothEffects({
+		executeSelectedHoundstoothEffects({
 			houndstoothOverrides: { basePattern: { gridSettings: { tileResolution: 8 } } },
 		})
 
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 0, from.Unit(areaSize) * 0 ]),
-			areaSize,
-			color: BLACK,
-			sectionAddress: to.Address([ 0, 0 ]),
-			sectionResolution: 1,
-		})).toBe(true)
+		setTimeout(() => {
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 0, from.Unit(areaSize) * 0 ]),
+				areaSize,
+				color: BLACK,
+				sectionAddress: to.Address([ 0, 0 ]),
+				sectionResolution: 1,
+			})).toBe(true)
 
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 1, from.Unit(areaSize) * 1 ]),
-			areaSize,
-			color: BLACK,
-			sectionAddress: to.Address([ 0, 0 ]),
-			sectionResolution: 2,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 1, from.Unit(areaSize) * 1 ]),
-			areaSize,
-			color: TRANSPARENT,
-			sectionAddress: to.Address([ 1, 1 ]),
-			sectionResolution: 2,
-		})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 1, from.Unit(areaSize) * 1 ]),
+				areaSize,
+				color: BLACK,
+				sectionAddress: to.Address([ 0, 0 ]),
+				sectionResolution: 2,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 1, from.Unit(areaSize) * 1 ]),
+				areaSize,
+				color: TRANSPARENT,
+				sectionAddress: to.Address([ 1, 1 ]),
+				sectionResolution: 2,
+			})).toBe(true)
 
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
-			areaSize,
-			color: TRANSPARENT,
-			sectionAddress: to.Address([ 0, 0 ]),
-			sectionResolution: 3,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
-			areaSize,
-			color: BLACK,
-			sectionAddress: to.Address([ 1, 1 ]),
-			sectionResolution: 3,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
-			areaSize,
-			color: TRANSPARENT,
-			sectionAddress: to.Address([ 2, 2 ]),
-			sectionResolution: 3,
-		})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
+				areaSize,
+				color: TRANSPARENT,
+				sectionAddress: to.Address([ 0, 0 ]),
+				sectionResolution: 3,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
+				areaSize,
+				color: BLACK,
+				sectionAddress: to.Address([ 1, 1 ]),
+				sectionResolution: 3,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 2, from.Unit(areaSize) * 2 ]),
+				areaSize,
+				color: TRANSPARENT,
+				sectionAddress: to.Address([ 2, 2 ]),
+				sectionResolution: 3,
+			})).toBe(true)
 
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
-			areaSize,
-			color: TRANSPARENT,
-			sectionAddress: to.Address([ 0, 0 ]),
-			sectionResolution: 4,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
-			areaSize,
-			color: BLACK,
-			sectionAddress: to.Address([ 1, 1 ]),
-			sectionResolution: 4,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
-			areaSize,
-			color: TRANSPARENT,
-			sectionAddress: to.Address([ 2, 2 ]),
-			sectionResolution: 4,
-		})).toBe(true)
-		expect(sectionCenterIsColor({
-			areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
-			areaSize,
-			color: BLACK,
-			sectionAddress: to.Address([ 3, 3 ]),
-			sectionResolution: 4,
-		})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
+				areaSize,
+				color: TRANSPARENT,
+				sectionAddress: to.Address([ 0, 0 ]),
+				sectionResolution: 4,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
+				areaSize,
+				color: BLACK,
+				sectionAddress: to.Address([ 1, 1 ]),
+				sectionResolution: 4,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
+				areaSize,
+				color: TRANSPARENT,
+				sectionAddress: to.Address([ 2, 2 ]),
+				sectionResolution: 4,
+			})).toBe(true)
+			expect(sectionCenterIsColor({
+				areaOrigin: to.Coordinate([ from.Unit(areaSize) * 3, from.Unit(areaSize) * 3 ]),
+				areaSize,
+				color: BLACK,
+				sectionAddress: to.Address([ 3, 3 ]),
+				sectionResolution: 4,
+			})).toBe(true)
 
-		done()
+			done()
+		},         0)
 	})
 
 	describe('animating', () => {
@@ -121,14 +123,16 @@ describe('gingham chevron continuum effect', () => {
 			activateTestMarkerCanvas()
 			state.animating = true
 
-			await executeSelectedHoundstoothEffects({ houndstoothOverrides })
+			executeSelectedHoundstoothEffects({ houndstoothOverrides })
 
-			expectStripedTile({ diagonalAddress: 0, stripeCount: 1, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 1, stripeCount: 2, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 2, stripeCount: 3, firstColor: TRANSPARENT })
-			expectStripedTile({ diagonalAddress: 3, stripeCount: 4, firstColor: TRANSPARENT })
+			setTimeout(() => {
+				expectStripedTile({ diagonalAddress: 0, stripeCount: 1, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 1, stripeCount: 2, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 2, stripeCount: 3, firstColor: TRANSPARENT })
+				expectStripedTile({ diagonalAddress: 3, stripeCount: 4, firstColor: TRANSPARENT })
 
-			done()
+				done()
+			},         0)
 		})
 
 		it('around frame 525 each tile has twice its original stripe count', async (done: DoneFn) => {
@@ -140,14 +144,16 @@ describe('gingham chevron continuum effect', () => {
 			activateTestMarkerCanvas()
 			state.animating = true
 
-			await executeSelectedHoundstoothEffects({ houndstoothOverrides })
+			executeSelectedHoundstoothEffects({ houndstoothOverrides })
 
-			expectStripedTile({ diagonalAddress: 0, stripeCount: 2, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 1, stripeCount: 4, firstColor: TRANSPARENT })
-			expectStripedTile({ diagonalAddress: 2, stripeCount: 6, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 3, stripeCount: 8, firstColor: TRANSPARENT })
+			setTimeout(() => {
+				expectStripedTile({ diagonalAddress: 0, stripeCount: 2, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 1, stripeCount: 4, firstColor: TRANSPARENT })
+				expectStripedTile({ diagonalAddress: 2, stripeCount: 6, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 3, stripeCount: 8, firstColor: TRANSPARENT })
 
-			done()
+				done()
+			},         0)
 		})
 
 		it('around frame 666 each tile has thrice its original stripe count', async (done: DoneFn) => {
@@ -159,14 +165,16 @@ describe('gingham chevron continuum effect', () => {
 			activateTestMarkerCanvas()
 			state.animating = true
 
-			await executeSelectedHoundstoothEffects({ houndstoothOverrides })
+			executeSelectedHoundstoothEffects({ houndstoothOverrides })
 
-			expectStripedTile({ diagonalAddress: 0, stripeCount: 3, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 1, stripeCount: 6, firstColor: BLACK })
-			expectStripedTile({ diagonalAddress: 2, stripeCount: 9, firstColor: TRANSPARENT })
-			expectStripedTile({ diagonalAddress: 3, stripeCount: 12, firstColor: TRANSPARENT })
+			setTimeout(() => {
+				expectStripedTile({ diagonalAddress: 0, stripeCount: 3, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 1, stripeCount: 6, firstColor: BLACK })
+				expectStripedTile({ diagonalAddress: 2, stripeCount: 9, firstColor: TRANSPARENT })
+				expectStripedTile({ diagonalAddress: 3, stripeCount: 12, firstColor: TRANSPARENT })
 
-			done()
+				done()
+			},         0)
 		})
 	})
 })
