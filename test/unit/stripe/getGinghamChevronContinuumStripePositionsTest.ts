1263eb37ef75bb66a94d48a467d7e8573361788d
Douglas <douglas.blumeyer@gmail.com>
2017-11-10 10:38:30 +0900

    fix broken unit test glob, and tests that had been secretly broken due to spies missing their marks, both due to module reorganization

--- test/unit/stripe/getGinghamChevronContinuumStripePositionsTest.ts
+++ test/unit/stripe/getGinghamChevronContinuumStripePositionsTest.ts
@@ -3,7 +3,7 @@
 import { composeMainHoundstooth } from '../../../../../src/app/execute/composeMainHoundstooth'
 import { StripePosition } from '../../../../../src/pattern/stripe/types'
 import * as to from '../../../../../src/to'
-import * as neededStripeCountToCoverGrid from '../../../pattern/grid/neededStripeCountToCoverGrid'
+import * as grid from '../../../pattern/grid'
 import { getGinghamChevronContinuumStripePositions } from '../../../pattern/stripe/getGinghamChevronContinuumStripePositions'
 
 describe('get gingham chevron continuum stripe positions returns an array of numbers representing the positions of the stripes for a given tile in terms of its perimeter (2 is the max) based on where on the grid it is', () => {
@@ -23,7 +23,7 @@ describe('get gingham chevron continuum stripe positions returns an array of num
 	})
 
 	it('edge case', () => {
-		spyOn(neededStripeCountToCoverGrid, 'neededStripeCountToCoverGrid').and.returnValue(0)
+		spyOn(grid, 'neededStripeCountToCoverGrid').and.returnValue(0)
 
 		const actual: StripePosition[] = getGinghamChevronContinuumStripePositions({ gridAddress: to.Address([ 1, 5 ]) })
 		expect(actual).toEqual(to.StripePositions([ 0 ]))
